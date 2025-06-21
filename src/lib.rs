26a6f30bfc5b90bf4b3ddef8cb6cbb1df02a3b92
Takeru Ohta <phjgt308@gmail.com>
2025-06-21 18:10:42 +0900

    Add tuinix_ext module with terminal frame and region utilities

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub mod app;
 pub mod buffer;
+pub mod editor;
+pub mod tuinix_ext;
 // pub mod widget_console;
 // pub mod widget_dialog;
 // pub mod widget_editor;
 // pub mod widget_notification;
 // pub mod widget_status;
 // pub mod widget_text;
-pub mod editor;
---
+++ src/tuinix_ext.rs
@@ -0,0 +1,58 @@
+use tuinix::{TerminalFrame, TerminalPosition, TerminalSize};
+
+pub trait TerminalFrameExt {
+    fn draw_in_region<F, E>(&mut self, region: TerminalRegion, f: F) -> Result<(), E>
+    where
+        F: FnOnce(&mut TerminalFrame) -> Result<(), E>;
+}
+
+impl TerminalFrameExt for TerminalFrame {
+    fn draw_in_region<F, E>(&mut self, region: TerminalRegion, f: F) -> Result<(), E>
+    where
+        F: FnOnce(&mut TerminalFrame) -> Result<(), E>,
+    {
+        let mut subframe = TerminalFrame::new(region.size);
+        f(&mut subframe)?;
+        self.draw(region.position, &subframe);
+        Ok(())
+    }
+}
+
+pub trait TerminalSizeExt {
+    fn to_region(self) -> TerminalRegion;
+}
+
+impl TerminalSizeExt for TerminalSize {
+    fn to_region(self) -> TerminalRegion {
+        TerminalRegion {
+            position: TerminalPosition::ZERO,
+            size: self,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct TerminalRegion {
+    pub position: TerminalPosition,
+    pub size: TerminalSize,
+}
+
+impl TerminalRegion {
+    pub fn top_rows(mut self, rows: usize) -> Self {
+        self.size.rows = self.size.rows.min(rows);
+        self
+    }
+
+    pub fn bottom_rows(mut self, rows: usize) -> Self {
+        if let Some(offset) = self.size.rows.checked_sub(rows) {
+            self.position.row += offset;
+            self.size.rows = rows;
+        }
+        self
+    }
+
+    pub fn without_bottom_rows(mut self, rows: usize) -> Self {
+        self.size.rows = self.size.rows.saturating_sub(rows);
+        self
+    }
+}
