bbb34a8b7eba0aee3e0141588b2bea14ec96d873
Takeru Ohta <phjgt308@gmail.com>
2025-06-22 08:13:33 +0900

    Implement TextBuffer with line storage and add empty widget_text module

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,12 +1,20 @@
 #[derive(Debug)]
-pub struct TextBuffer {}
+pub struct TextBuffer {
+    lines: Vec<String>,
+}
 
 impl TextBuffer {
     pub fn new() -> Self {
-        Self {}
+        Self { lines: Vec::new() }
     }
 
-    pub fn set_text(&mut self, text: String) {}
+    pub fn lines(&self) -> impl '_ + Iterator<Item = &str> {
+        self.lines.iter().map(|s| s.as_ref())
+    }
+
+    pub fn set_text(&mut self, text: String) {
+        self.lines = text.lines().map(|s| s.to_owned()).collect();
+    }
 }
 
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
