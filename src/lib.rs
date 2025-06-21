e9f3887544d26e9908c0186b72ca2b81153fa49a
Takeru Ohta <phjgt308@gmail.com>
2025-06-21 18:08:45 +0900

    Remove editor module and add commented widget module declarations

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 pub mod app;
 pub mod buffer;
-pub mod editor;
+// pub mod widget_console;
+// pub mod widget_dialog;
+// pub mod widget_editor;
+// pub mod widget_notification;
+// pub mod widget_status;
+// pub mod widget_text;
