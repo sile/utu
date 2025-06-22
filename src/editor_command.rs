c3b985af6f88dfa2c0038463481835e1b8843396
Takeru Ohta <phjgt308@gmail.com>
2025-06-22 19:31:02 +0900

    Add ToggleLegend command with C-h keybinding

--- src/editor_command.rs
+++ src/editor_command.rs
@@ -1,6 +1,7 @@
 #[derive(Debug, Clone, Copy)]
 pub enum EditorCommand {
     Quit,
+    ToggleLegend,
     Cancel,
     PrevLine,
     NextLine,
@@ -17,6 +18,7 @@ impl std::fmt::Display for EditorCommand {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             EditorCommand::Quit => write!(f, "quit"),
+            EditorCommand::ToggleLegend => write!(f, "toggle-legend"),
             EditorCommand::Cancel => write!(f, "cancel"),
             EditorCommand::PrevLine => write!(f, "prev-line"),
             EditorCommand::NextLine => write!(f, "next-line"),
@@ -37,6 +39,7 @@ impl std::str::FromStr for EditorCommand {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
             "quit" => Ok(EditorCommand::Quit),
+            "toggle-legend" => Ok(EditorCommand::ToggleLegend),
             "cancel" => Ok(EditorCommand::Cancel),
             "prev-line" => Ok(EditorCommand::PrevLine),
             "next-line" => Ok(EditorCommand::NextLine),
