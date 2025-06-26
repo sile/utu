e6b09ae16ad1adc9335a45c065a74e62858a1352
Takeru Ohta <phjgt308@gmail.com>
2025-06-26 10:27:26 +0900

    Add KeyBindings and KeyBindingsGroup structs with JSON parsing support

--- src/keybinding2.rs
+++ src/keybinding2.rs
@@ -5,6 +5,24 @@ use nojson::{JsonParseError, RawJsonValue};
 use crate::nojson_ext::RawJsonValueExt;
 
 #[derive(Debug)]
+pub struct KeyBindings {
+    pub main: KeyBindingsGroup,
+    pub groups: BTreeMap<String, KeyBindingsGroup>,
+}
+
+impl<'text> nojson::FromRawJsonValue<'text> for KeyBindings {
+    fn from_raw_json_value(
+        value: nojson::RawJsonValue<'text, '_>,
+    ) -> Result<Self, nojson::JsonParseError> {
+        let raw = RawKeyBindings::parse(value)?;
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+pub struct KeyBindingsGroup {}
+
+#[derive(Debug)]
 pub struct RawKeyBindings<'text, 'a> {
     pub root: RawJsonValue<'text, 'a>,
     pub groups: BTreeMap<String, RawKeyBindingsGroup<'text, 'a>>,
@@ -53,6 +71,7 @@ pub struct RawKeyBindingEntry<'text, 'a> {
 
 #[cfg(test)]
 mod tests {
+    use nojson::FromRawJsonValue;
     use orfail::OrFail;
 
     use super::*;
@@ -66,6 +85,18 @@ mod tests {
             .to_fixed_object(["keybindings"], [])
             .or_fail()?;
         RawKeyBindings::parse(keybindings).or_fail()?;
+        Ok(())
+    }
+
+    #[test]
+    fn parse_key_bindings() -> orfail::Result<()> {
+        let json = include_str!("../default.config.json");
+        let json = nojson::RawJson::parse(json).or_fail()?;
+        let ([keybindings], []) = json
+            .value()
+            .to_fixed_object(["keybindings"], [])
+            .or_fail()?;
+        KeyBindings::from_raw_json_value(keybindings).or_fail()?;
         Ok(())
     }
 }
